6a1b208f193270ed943e9f861b2b1d5c11c5139f
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2016-02-02 13:44:51 -0500

    Update download_GPM.R

--- download_GPM.R
+++ download_GPM.R
@@ -1,4 +1,4 @@
-#' Download and convert GPM Product data in GeoTIFF
+#' Download and convert GPM Product data in GeoTIFF version 2.0
 #' @author Cesar Aybar Camacho 
 #' @description This is the preliminary version for massive downloading data GPM.  
 #' @details You need to have installed "Axel download accelerator application" and gdal.
@@ -6,19 +6,13 @@
 #'          Download Linux axel: apt-get install axel
 #'          Download windows axel: https://st0rage.org/~n2j3/?page_id=225717166
 #' @param fileLocation: file path where to save.
-#' @param url: ftp where data is stored
-#'        soported: 
-#'              -ftp://jsimpson.pps.eosdis.nasa.gov/data/imerg/" 
-#'              -"ftp://arthurhou.pps.eosdis.nasa.gov/gpmdata/" (for default)
-#'              -http://gpm1.gesdisc.eosdis.nasa.gov/opendap/GPM_L3/GPM_3IMERGHH.03/ (comming soon)
 #' @param user: enter your user.
 #' @param password: enter your password.
-#' @param formatDownL: Indicate the data format download (soported "HDF5" or "geoTIFF")
-#' @param formatSave: Indicate the data format  save "HDF5" or "geoTIIF".
-#' @param latency:  default is "early" (only valid in ftp://jsimpson.pps.eosdis.nasa.gov)
-#' @param year: year of interest
-#' @param month: month of interest
-#' @param day: day of interest
+#' @param typeProduct:  default is "finalrun" 
+#' @param dates: defines the discharge range
+#' - "Early" period that has now data (2015-03-31- present).
+#' - "Late": Period currently has data (31.03.2015).
+#' - "Finalrun" which features data Period (12/03/2014 to 2015-09).
 #' @param band: 
 #'              band "HQobservationTime" = 1
 #'              band "HQprecipSource" = 2
@@ -29,32 +23,28 @@
 #'              band "precipitationUncal" = 7
 #'              band "probabilityLiquidPrecipitation" = 8
 #'              band "randomError" = 9  
-#' @param paralelled:  (comming soon)
 #' @param BBlonMin Minimum latitude of bounding box
 #' @param BBlonMax Maximum latitude of bounding box
 #' @param BBlatMin Minimum longitude of bounding box
 #' @param BBlatMax Maximum longitude of bounding box
-#' @details This code is based in Martin Brandt's blog post and Claudia Vitolo Github: 
-#' http://matinbrandt.wordpress.com/2013/09/04/automatically-downloading-and-processing-trmm-rainfall-data/
+#' @details This code is based on the script  "ModisDownload.R" created by Babak Naimi and "TRMM.R" Claudia Vitolo: 
 #' https://gist.github.com/cvitolo/9b5322be6fb940cc718a
+#' Use of the PPS FTP to download GPM and TRMM data is free, but you are required to first register your email address 
+#' http://registration.pps.eosdis.nasa.gov/registration/
 #' more information about GPM see ftp://jsimpson.pps.eosdis.nasa.gov/data/imerg/IMERG_ATBD_V4.5.pdf
 #' more information about Axel see http://www.linuxjournal.com/content/speed-your-downloads-axel
 #' @examples 
-#' downloadGPM(fiLocation = "/home/UseX/gpm/")
+#' #downloadGPM(fileLocation = "C:/Users/Papita/Desktop/prueb/lat/",user = "xxx",password = "xxx",typeProduct = "late",dates = c('2015.05.01','2015.05.02'))
 #'
-
-downloadGPM <- function(fileLocation = "/home/senamhi-cesar/Escritorio/downloadgpm/",
-                        url = "ftp://arthurhou.pps.eosdis.nasa.gov/gpmdata/",
-                        formatDownL="HDF5",
-                        formatSave="geoTIFF",
-                        year="2014",
-                        month="03",
-                        day="12",
-                        latency = "early",
-                        user="XXX@gmail.com",
-                        password="XXX@gmail.com",
+#rm(list = ls())
+downloadGPM <- function(fileLocation = "/home/UserX/folder1/",
+                        user="xxx",
+                        password="xxx",
+                        dates=c('2014.05.01','2014.05.02'),
+                        typeProduct = "finalrun",
+                        maxspeeddownload= 504800, # 504,800 Bytes/second (500KB/s)
+                        numconnections='3',
                         band=6,
-                        paralelled=T,
                         BBlonMin = -86,
                         BBlonMax = -66,
                         BBlatMin = -19.25,
@@ -64,81 +54,75 @@ downloadGPM <- function(fileLocation = "/home/senamhi-cesar/Escritorio/downloadg
   if (!require(RCurl)) stop("Package RCurl is not installed")
   if (!require(raster)) stop("Package raster is not installed")
   if (!require(rgdal)) stop("Package rgdal is not installed")
-  if (!require(gdalUtils)) stop("Package raster is not installed")
-  #require(foreach)
+  if (!require(gdalUtils)) stop("Package gdalUtils is not installed")
   
-  setwd(fileLocation)
-  dir.create(paste(year,"-",month,"-",day,sep=""))
-  setwd(paste(fileLocation,year,"-",month,"-",day,sep=""))
+  if(!inherits(dates,"character")) stop("your date is not a character!")
+  if (inherits(dates,"character")) dates <- as.Date(dates,format='%Y.%m.%d')
+  dates <- na.omit(as.Date(dates))
+  if (length(dates) == 0) stop("dates is not appropriately selected!")
+  dates <- sort(dates) 
+  timeseries<-seq(from=dates[1],to=dates[2],by='day')
+  #--------
   
-  if(url == "ftp://jsimpson.pps.eosdis.nasa.gov/data/imerg/"){
-    
-    myURL <-paste(url,"/",year,month,"/",sep="")
-    filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
-    filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
-    selectedfilePaths <- filePaths[grep(filePaths,pattern=paste("*",year,month,day,"-",sep=""))] 
-    x<- function(Url,bs)  system(paste("axel -n 3 ",Url," -o ",paste(fileLocation,year,"-",month,"-",day,sep=""),"/",bs,sep=""))
-    mapply(x,paste("ftp://",user,":",password,"@",substr(selectedfilePaths,7,150),sep=""),basename(selectedfilePaths))}  
-  
-  if(url == "ftp://arthurhou.pps.eosdis.nasa.gov/gpmdata/"){
-    if(formatDownL == "HDF5"){
-      myURL <-paste(url,"/",year,"/",month,"/",day,"/imerg/",sep="")
-      filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
+  .gpmdownloadI<- function(day=1){ 
+    if (trim(typeProduct)=='finalrun'){ 
+      myURL <-paste0("ftp://arthurhou.pps.eosdis.nasa.gov/gpmdata","/",format(timeseries[day], "%Y"),"/",format(timeseries[day], "%m"),
+                     "/",format(timeseries[day], "%d"),"/imerg/")
+      filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, 
+                          crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
       filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
       selectedfilePaths <- filePaths[grep(filePaths,pattern="*.HDF5")] 
-      
-      x<- function(Url,bs)  system(paste("axel -n 3 ",Url," -o ",paste(fileLocation,year,"-",month,"-",day,sep=""),"/",bs,sep=""))
-      mapply(x,paste("ftp://",user,":",password,"@",substr(selectedfilePaths,7,150),sep=""),basename(selectedfilePaths))
-    }  
+      selectedfilePaths <- sort(selectedfilePaths)
+      selectedfilePaths<-selectedfilePaths[-49]}
     
-    if(formatDownL == "GIS"){
-      myURL <-paste(url,"/",year,"/",month,"/",day,"/gis/",sep="")
+    
+    if (trim(typeProduct)=='late'){ 
+      myURL <-paste("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/late","/",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),"/",sep="")
       filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
       filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
-      selectedfilePaths <- filePaths[grep(filePaths,pattern="*\\.tif$")] 
-      x<- function(Url,bs)  system(paste("axel -n 3 ",Url," -o ",paste(fileLocation,year,"-",month,"-",day,sep=""),"/",bs,sep=""))
-      mapply(x,paste("ftp://",user,":",password,"@",substr(selectedfilePaths,7,150),sep=""),basename(selectedfilePaths))}  
-  }
-  
-  if (formatSave=="HDF5")  message(paste0("Download finish for the  ",year,"-",month,"-",day))
-  baseraster <- raster(extent(-180,180,-90,90))
-  res(baseraster) <- 0.1
-  projection(baseraster) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-  
-  if(formatSave=="geoTIFF")
-    if(substr(list.files(),61,63)[1]=="tif"){ 
-      file_path<-paste0(paste(fileLocation,"/",year,"-",month,"-",day,sep=""),"/",basename(list.files()))   
-      
-      z<-function(k){ 
-        rl1<-as.matrix(raster(file_path[k]))
-        mtrx = matrix(rl1, nrow=1800, ncol=3600)
-        baseraster[]<-mtrx  
-        prec<-crop(baseraster,extent(BBlonMin,BBlonMax,BBlatMin,BBlatMax) )
-        prec<-prec*0.05
-        writeRaster(prec,file_path[k],overwrite=T)}
-      mapply(z,1:48)}
-  
-  
-  if(substr(list.files(),57,63)[1]=="HDF5"){ 
-    file_path<-paste0(paste(fileLocation,"/",year,"-",month,"-",day,sep=""),"/",basename(list.files()))   
+      if(day<10)  day2<-paste0("0",day)
+      selectedfilePaths <- filePaths[grep(filePaths,pattern=paste("*",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),day2,"-",sep=""))]}
     
-    y<- function(a,b) system(paste0('gdal_translate ',get_subdatasets(a)[band]," ",b,"-",band,".tif"))
-    mapply(y,file_path,basename(file_path))
+    if (trim(typeProduct)=='early'){ 
+      myURL <-paste("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/early","/",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),"/",sep="")
+      filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
+      filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
+      if(day<10)  day2<-paste0("0",day)
+      selectedfilePaths <- filePaths[grep(filePaths,pattern=paste("*",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),day2,"-",sep=""))]}
     
-    baseraster <- raster(extent(-180,180,-90,90))
-    res(baseraster) <- 0.1
-    projection(baseraster) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-    rl<-list.files(pattern = ".tif")
+    setwd(trim(fileLocation))
+    dir.create(paste(format(timeseries[day], "%Y"),"-",format(timeseries[day], "%m")
+                     ,"-",format(timeseries[day], "%d"),sep=""))
     
-    z<-function(k){ 
-      rl1<-t(as.matrix(raster(k)))
+    setwd(paste(fileLocation,format(timeseries[day], "%Y"),
+                "-",format(timeseries[day], "%m"),"-",format(timeseries[day], "%d"),sep=""))
+    
+    down<- function(Url,bs)  system(paste0("axel -n ",numconnections,
+                                           " -s ",maxspeeddownload," ",Url," -o ",paste(fileLocation,format(timeseries[day], "%Y"),
+                                                                                        "-",format(timeseries[day], "%m"),"-",format(timeseries[day], "%d"),sep=""),"/",bs))
+    #--------
+    
+    .gpmdownloadII<-function(timeS=1){
+      selectedfilePaths2<- selectedfilePaths[timeS]
+      down(Url = paste0("ftp://",user,":",password,"@",substr(selectedfilePaths2,7,150)),bs = basename(selectedfilePaths2))
+      y<- function(a,b) system(paste0('gdal_translate ',get_subdatasets(a)[band]," ",b,"-",band,".tif"))  
+      file_path<-paste0(paste(fileLocation,"/",format(timeseries[day], "%Y"),
+                              "-",format(timeseries[day], "%m"),"-",
+                              format(timeseries[day], "%d"),sep=""),"/",basename(list.files())[length(basename(list.files()))])
+      y(file_path,basename(file_path))
+      baseraster <- raster(extent(-180,180,-90,90))
+      res(baseraster) <- 0.1
+      projection(baseraster) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+      rl<-list.files(pattern = ".tif")[length(list.files(pattern = ".tif"))]
+      rl1<-t(as.matrix(raster(rl)))
       mtrx = matrix(rl1, nrow=1800, ncol=3600)
       baseraster[]<-mtrx  
       flp<-flip(baseraster,direction = "y")
       prec<-crop(flp,extent(BBlonMin,BBlonMax,BBlatMin,BBlatMax) )
-      writeRaster(prec/2,k,overwrite=T)
+      writeRaster(prec/2,rl,overwrite=T)
+      file.remove(file_path)
     }
-    mapply(z,rl)
-    file.remove(file_path)
+    mapply(.gpmdownloadII,1:48)
   }
+  mapply(.gpmdownloadI,1:length(timeseries))  
 }
