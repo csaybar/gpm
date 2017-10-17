ee9e902acc0fd138d31bc4ba6ac898447f14bdca
cesar <aybar1994@gmail.com>
2017-10-17 15:04:58 -0500

    download_GPM.R bug fixes
    -Tested only in linux.

--- download_GPM.R
+++ download_GPM.R
@@ -37,79 +37,105 @@
 #' #downloadGPM(fileLocation = "C:/Users/Papita/Desktop/prueb/lat/",user = "xxx",password = "xxx",typeProduct = "late",dates = c('2015.05.01','2015.05.02'))
 #'
 #rm(list = ls())
+# library(lubridate)
+# library(tidyverse)
+
 downloadGPM <- function(fileLocation = "/home/UserX/folder1/",
                         user="xxx",
                         password="xxx",
                         dates=c('2014.05.01','2014.05.02'),
                         typeProduct = "finalrun",
-                        maxspeeddownload= 504800, # 504,800 Bytes/second (500KB/s)
-                        numconnections='3',
+                        maxspeeddownload= 2004800, # 504,800 Bytes/second (500KB/s)
+                        numconnections='5',
+                        init="1200",
                         band=6,
                         BBlonMin = -86,
                         BBlonMax = -66,
                         BBlatMin = -19.25,
                         BBlatMax = 1.25
 ){
-  
+  if (!require(tidyverse)) stop("Package tidyverse is not installed")
+  if (!require(lubridate)) stop("Package lubridate is not installed")
+  if (!require(tidyverse)) stop("Package tidyverse is not installed")
   if (!require(RCurl)) stop("Package RCurl is not installed")
   if (!require(raster)) stop("Package raster is not installed")
   if (!require(rgdal)) stop("Package rgdal is not installed")
   if (!require(gdalUtils)) stop("Package gdalUtils is not installed")
-  
-  if(!inherits(dates,"character")) stop("your date is not a character!")
-  if (inherits(dates,"character")) dates <- as.Date(dates,format='%Y.%m.%d')
-  dates <- na.omit(as.Date(dates))
-  if (length(dates) == 0) stop("dates is not appropriately selected!")
-  dates <- sort(dates) 
-  timeseries<-seq(from=dates[1],to=dates[2],by='day')
+  if(!inherits(dates,"Date")) stop("your date is not a Date!")
+  timeseries<-dates
   #--------
-  
-  .gpmdownloadI<- function(day=1){ 
-    if (trim(typeProduct)=='finalrun'){ 
-      myURL <-paste0("ftp://arthurhou.pps.eosdis.nasa.gov/gpmdata","/",format(timeseries[day], "%Y"),"/",format(timeseries[day], "%m"),
-                     "/",format(timeseries[day], "%d"),"/imerg/")
-      filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, 
-                          crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
-      filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
-      selectedfilePaths <- filePaths[grep(filePaths,pattern="*.HDF5")] 
-      selectedfilePaths <- sort(selectedfilePaths)
-      selectedfilePaths<-selectedfilePaths[-49]}
+  for(d in 1:length(timeseries)){
     
+    year2=c(year(timeseries[d]),year(timeseries[d]+1))
+    month2=c(month(timeseries[d]),month(timeseries[d]+1))
+    day2=c(day(timeseries[d]),day(timeseries[d]+1))
     
-    if (trim(typeProduct)=='late'){ 
-      myURL <-paste("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/late","/",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),"/",sep="")
-      filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
-      filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
-      if(day<10)  day2<-paste0("0",day)
-      selectedfilePaths <- filePaths[grep(filePaths,pattern=paste("*",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),day2,"-",sep=""))]}
+    if (trim(typeProduct)=='finalrun'){
+      myURL <- lapply(1:2, function(i) sprintf("ftp://arthurhou.pps.eosdis.nasa.gov/gpmdata/%s/%02d/%02d/imerg/",
+                                               year2[i],month2[i],day2[i]))
+      filenames<-try(lapply(1:2,function(i) getURL(myURL[[i]], ftp.use.epsv = FALSE, ftplistonly=TRUE,
+                                                   crlf=TRUE,userpwd=sprintf("%s:%s",user,password))))
+      if(class(filenames)=="try-error") stop("Problem in the IMERG-finalrun download")
+      filePaths <-  lapply(1:2, function(i) paste(myURL[[i]], strsplit(filenames[[i]], "\r*\n")[[1]], sep=""))
+      filePaths <- sort(do.call("c",filePaths))
+      position_init=grep(sprintf("*%s%02d%02d-S%s",year2[1],month2[1],day2[1],init),filePaths)
+      selectedfilePaths <- filePaths[position_init:(position_init+47)]
+    }
     
-    if (trim(typeProduct)=='early'){ 
-      myURL <-paste("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/early","/",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),"/",sep="")
-      filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd="aybar1994@gmail.com:aybar1994@gmail.com")
-      filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
-      if(day<10)  day2<-paste0("0",day)
-      selectedfilePaths <- filePaths[grep(filePaths,pattern=paste("*",format(timeseries[day], "%Y"),format(timeseries[day], "%m"),day2,"-",sep=""))]}
+    if (trim(typeProduct)=='late'){
+      if(month2[1]==month2[2]){
+        myURL <- sprintf("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/late/%s%02d/",year2[1],month2[1])
+        filenames<-getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd=sprintf("%s:%s",user,password))
+        filePaths <-  paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
+        position_init <- grep(sprintf("*%s%02d%02d-S%s",year2[1],month2[1],day2[1],init),filePaths)
+        selectedfilePaths <- filePaths[position_init:(position_init+47)]  
+      }
+      else{
+        myURL <- lapply(1:2, function(i) sprintf("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/late/%s%02d/",year2[i],month2[i]))
+        filenames<-lapply(1:2,function(i) getURL(myURL[[i]], ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd=sprintf("%s:%s",user,password)))
+        filePaths <-  lapply(1:2, function(i) paste(myURL[[i]], strsplit(filenames[[i]], "\r*\n")[[1]], sep=""))
+        filePaths <- sort(do.call("c",filePaths))
+        position_init=grep(sprintf("*%s%02d%02d-S%s",year2[1],month2[1],day2[1],init),filePaths)
+        selectedfilePaths <- filePaths[position_init:(position_init+47)]
+      }
+    }
+    if (trim(typeProduct)=='early'){
+      if(month2[1]==month2[2]){
+        myURL <- sprintf("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/early/%s%02d/",year2[1],month2[1])
+        filenames<-getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd=sprintf("%s:%s",user,password))
+        filePaths <-  paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
+        position_init <- grep(sprintf("*%s%02d%02d-S%s",year2[1],month2[1],day2[1],init),filePaths)
+        selectedfilePaths <- filePaths[position_init:(position_init+47)]  
+      }
+      else{
+        myURL <- lapply(1:2, function(i) sprintf("ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/early/%s%02d/",year2[i],month2[i]))
+        filenames<-lapply(1:2,function(i) getURL(myURL[[i]], ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE,userpwd=sprintf("%s:%s",user,password)))
+        filePaths <-  lapply(1:2, function(i) paste(myURL[[i]], strsplit(filenames[[i]], "\r*\n")[[1]], sep=""))
+        filePaths <- sort(do.call("c",filePaths))
+        position_init=grep(sprintf("*%s%02d%02d-S%s",year2[1],month2[1],day2[1],init),filePaths)
+        selectedfilePaths <- filePaths[position_init:(position_init+47)]
+      }
+    }
     
+    bs = basename(selectedfilePaths)
     setwd(trim(fileLocation))
-    dir.create(paste(format(timeseries[day], "%Y"),"-",format(timeseries[day], "%m")
-                     ,"-",format(timeseries[day], "%d"),sep=""))
+    dir.create(sprintf("%s-%02d-%02d",year2[1],month2[1],day2[1]))
+    setwd(sprintf("%s/%s-%02d-%02d",trim(fileLocation),year2[1],month2[1],day2[1]))
+    Url = sprintf("ftp://%s:%s@%s",user,password,gsub("ftp://","",selectedfilePaths))
+    bs = basename(selectedfilePaths)
     
-    setwd(paste(fileLocation,format(timeseries[day], "%Y"),
-                "-",format(timeseries[day], "%m"),"-",format(timeseries[day], "%d"),sep=""))
-    
-    down<- function(Url,bs)  system(paste0("axel -n ",numconnections,
-                                           " -s ",maxspeeddownload," ",Url," -o ",paste(fileLocation,format(timeseries[day], "%Y"),
-                                                                                        "-",format(timeseries[day], "%m"),"-",format(timeseries[day], "%d"),sep=""),"/",bs))
+    down<- function(i)  system(sprintf("axel -n %s -s %s %s -o %s/%s-%02d-%02d/%s",numconnections,
+                                       maxspeeddownload,Url[i],trim(fileLocation),year2[1],month2[1],day2[1],bs[i]))
+    y<- function(a,b) system(paste0('gdal_translate ',get_subdatasets(a)[band]," ",b,"-",band,".tif"))  
     #--------
     
-    .gpmdownloadII<-function(timeS=1){
-      selectedfilePaths2<- selectedfilePaths[timeS]
-      down(Url = paste0("ftp://",user,":",password,"@",substr(selectedfilePaths2,7,150)),bs = basename(selectedfilePaths2))
-      y<- function(a,b) system(paste0('gdal_translate ',get_subdatasets(a)[band]," ",b,"-",band,".tif"))  
-      file_path<-paste0(paste(fileLocation,"/",format(timeseries[day], "%Y"),
-                              "-",format(timeseries[day], "%m"),"-",
-                              format(timeseries[day], "%d"),sep=""),"/",basename(list.files())[length(basename(list.files()))])
-      y(file_path,basename(file_path))
+    for(i in 1:48){
+      repeat{
+        down(i)
+        file_path<-sprintf("%s/%s-%02d-%02d/%s",trim(fileLocation),year2[1],month2[1],day2[1],bs[i])
+        a=try(y(file_path,basename(file_path)))
+        if(!class(a)=="try-error") break
+      }
       baseraster <- raster(extent(-180,180,-90,90))
       res(baseraster) <- 0.1
       projection(baseraster) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
@@ -118,11 +144,16 @@ downloadGPM <- function(fileLocation = "/home/UserX/folder1/",
       mtrx = matrix(rl1, nrow=1800, ncol=3600)
       baseraster[]<-mtrx  
       flp<-flip(baseraster,direction = "y")
-      prec<-crop(flp,extent(BBlonMin,BBlonMax,BBlatMin,BBlatMax) )
+      prec<-crop(flp,extent(BBlonMin,BBlonMax,BBlatMin,BBlatMax))
       writeRaster(prec/2,rl,overwrite=T)
       file.remove(file_path)
     }
-    mapply(.gpmdownloadII,1:48)
   }
-  mapply(.gpmdownloadI,1:length(timeseries))  
 }
+
+#Example
+# downloadGPM(fileLocation = "/home/.../",
+#             user = "x@gmail.com",
+#             password = "x@gmail.com",
+#             typeProduct = "early",
+#             dates = seq(as.Date('2016-07-31'),as.Date('2016-10-02'),"day"))
